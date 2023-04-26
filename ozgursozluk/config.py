f5013e213c614c9557b9284adebff47478df89d3
beucismis <beucismis@tutamail.com>
2023-04-27 00:11:32 +0300

    Update config.py

--- ozgursozluk/config.py
+++ ozgursozluk/config.py
@@ -4,5 +4,5 @@ from os import environ
 SECRET_KEY = environ.get("OZGURSOZLUK_SECRET_KEY", "Some secret string")
 
 DEFAULT_THEME = "light"
-DEFAULT_DISPLAY_AUTHOR_NICKNAME = "False"
+DEFAULT_DISPLAY_AUTHOR_NICKNAME = "false"
 DEFAULT_EKSI_BASE_URL = "https://eksisozluk.com"
