967b589fa53d0ad3da069c7ec3ee05afdeeea87d
beucismis <beucismis@tutamail.com>
2023-04-28 20:15:42 +0300

    General fix

--- ozgursozluk/__init__.py
+++ ozgursozluk/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 
-__version__ = "0.4.1"
+__version__ = "0.4.2"
 __author__ = "beucismis"
 __source__ = "https://github.com/beucismis/ozgursozluk"
 __description__ = "free alternative simple ekşi sözlük front-end"
