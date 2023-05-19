8398a2d61d2cdee204d51ff0ae949ea094788e28
beucismis <beucismis@tutamail.com>
2023-05-19 15:46:13 +0300

    Bump version

--- ozgursozluk/__init__.py
+++ ozgursozluk/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 
-__version__ = "0.6.0"
+__version__ = "0.6.1"
 __author__ = "beucismis"
 __source__ = "https://github.com/beucismis/ozgursozluk"
 __description__ = "a free and open source alternative ekşi sözlük front-end"
