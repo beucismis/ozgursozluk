444a5b3f99a65e2d6430380c1b934bf084776ad9
beucismis <beucismis@tutamail.com>
2023-05-24 18:40:34 +0300

    Fix file name

--- ozgursozluk/__init__.py
+++ ozgursozluk/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask
 
 
-__version__ = "0.6.2"
+__version__ = "0.6.3"
 __author__ = "beucismis"
 __source__ = "https://github.com/beucismis/ozgursozluk"
 __description__ = "a free and open source alternative ekşi sözlük front-end"
 
 app = Flask(__name__)
-app.config.from_object("ozgursozluk.config")
+app.config.from_object("ozgursozluk.configs")
 
 
 import ozgursozluk.views
