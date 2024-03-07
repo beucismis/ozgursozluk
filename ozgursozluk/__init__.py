eee0869bb1abc2a3a25cac54aa18c5688fe50ad1
beucismis <beucismis@tutamail.com>
2024-03-08 02:01:07 +0300

    Version bump

--- ozgursozluk/__init__.py
+++ ozgursozluk/__init__.py
@@ -3,9 +3,9 @@ from flask import Flask
 from ozgursozluk.configs import SECRET_KEY
 
 
-__version__ = "0.8.0"
+__version__ = "0.8.1"
 __source_code__ = "https://github.com/beucismis/ozgursozluk"
-__description__ = "a free and open source alternative ekşi sözlük front-end"
+__description__ = "A free and open source alternative ekşi sözlük front-end"
 
 app = Flask(__name__)
 app.secret_key = SECRET_KEY
