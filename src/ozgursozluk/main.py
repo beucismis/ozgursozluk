177346670b3f570a12bda97050ce8c4fdcd20158
beucismis <beucismis@tutamail.com>
2025-09-15 13:03:57 +0300

    Update main.py

--- src/ozgursozluk/main.py
+++ src/ozgursozluk/main.py
@@ -1,10 +1,27 @@
+from datetime import datetime, UTC
+
 import flask
 
-from . import configs
+from . import __version__, configs
 
 
 app = flask.Flask(__name__)
 app.secret_key = configs.SECRET_KEY
+
+
+@app.route("/healtcheck")
+def healtcheck() -> flask.Response:
+    return flask.jsonify(status="healthy", version=__version__, timestamp=datetime.now(UTC))
+
+
+@app.route("/privacy-policy")
+def privacy_policy() -> str:
+    return flask.render_template("privacy-policy.html")
+
+
+@app.route("/terms-of-service")
+def terms_of_service() -> str:
+    return flask.render_template("terms-of-service.html")
 
 
 from . import views
