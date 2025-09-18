816f7b704d10326419435c19ddbf9003ad962d30
beucismis <beucismis@tutamail.com>
2025-09-18 09:49:08 +0300

    Update views.py

--- src/ozgursozluk/views.py
+++ src/ozgursozluk/views.py
@@ -4,6 +4,7 @@ from typing import NoReturn, Union
 
 import flask
 import limoon
+import werkzeug
 import requests
 from limoon.__about__ import __version__ as limoon_version
 
@@ -22,12 +23,23 @@ def global_template_variables() -> dict:
 
 @main.app.errorhandler(requests.ConnectTimeout)
 @main.app.errorhandler(requests.ConnectionError)
-def handle_index_not_found(e) -> tuple[str, int]:
-    return flask.render_template("not-found.html", description="API connection error, please reload page!"), 404
+def handle_connection_error(e) -> tuple[str, int]:
+    return (
+        flask.render_template("not-found.html", description="Connection error, please reload page!"),
+        503,
+    )
+
+
+@main.app.errorhandler(limoon.ElementNotFound)
+def handle_element_error(e) -> tuple[str, int]:
+    return (
+        flask.render_template("not-found.html", description=e.__doc__, message=e.message, html=e.html),
+        503,
+    )
 
 
 @main.app.route("/", methods=["GET", "POST"])
-def index() -> Union[str, flask.Response]:
+def index() -> Union[str, werkzeug.wrappers.Response]:
     q = flask.request.args.get("q", default=None, type=str)
     p = flask.request.args.get("p", default=1, type=int)
 
@@ -60,7 +72,10 @@ def topic(path: str) -> str:
 
 @main.app.errorhandler(limoon.EntryNotFound)
 def handle_entry_not_found(e) -> tuple[str, int]:
-    return flask.render_template("not-found.html", description=limoon.EntryNotFound.__doc__), 404
+    return (
+        flask.render_template("not-found.html", description=limoon.EntryNotFound.__doc__),
+        404,
+    )
 
 
 @main.app.route("/entry/<int:id>")
@@ -70,9 +85,12 @@ def entry(id: int) -> str:
     return flask.render_template("entry.html", entry=entry)
 
 
-@main.app.errorhandler(AttributeError)
+@main.app.errorhandler(limoon.AuthorNotFound)
 def handle_author_not_found(e) -> tuple[str, int]:
-    return flask.render_template("not-found.html", description=limoon.AuthorNotFound.__doc__), 404
+    return (
+        flask.render_template("not-found.html", description=limoon.AuthorNotFound.__doc__),
+        404,
+    )
 
 
 @main.app.route("/biri/<nickname>")
@@ -83,16 +101,19 @@ def author(nickname: str) -> str:
     return flask.render_template("author.html", author=author, author_last_entrys=author_last_entrys)
 
 
-@main.app.errorhandler(AttributeError)
+@main.app.errorhandler(limoon.SearchResultNotFound)
 def handle_search_result_not_found(e) -> tuple[str, int]:
-    return flask.render_template("not-found.html", description=limoon.SearchResultNotFound.__doc__), 404
+    return (
+        flask.render_template("not-found.html", description=limoon.SearchResultNotFound.__doc__),
+        404,
+    )
 
 
 @main.app.route("/search")
 def search() -> Union[str, NoReturn]:
     q = flask.request.args.get("q", default=None, type=str)
 
-    if q is None or not bool(len(q)):
+    if not q:
         return flask.abort(404, description=limoon.SearchResultNotFound.__doc__)
 
     search_result = limoon.get_search_topic(q)
@@ -100,42 +121,43 @@ def search() -> Union[str, NoReturn]:
     return flask.render_template("search.html", search_result=search_result, q=q)
 
 
-@main.app.route("/rentry")
-def rentry() -> flask.Response:
-    return flask.redirect(flask.url_for("entry", id=random.randint(1, 300_000_000)))
+@main.app.route("/external/rentry")
+def rentry() -> str:
+    entry = limoon.get_random_entry()
+
+    return flask.render_template("entry.html", entry=entry)
 
 
-@main.app.route("/about")
+@main.app.route("/external/about")
 def about() -> str:
     return flask.render_template("about.html")
 
 
-@main.app.route("/settings", methods=["GET", "POST"])
-def settings() -> Union[str, flask.Response]:
+@main.app.route("/external/settings", methods=["GET", "POST"])
+def settings() -> Union[str, werkzeug.wrappers.Response]:
     if flask.request.method == "POST":
         response = flask.redirect(flask.url_for("settings"))
 
-        for cookie in configs.DEFAULT_COOKIES:
-            if cookie == "theme":
-                value = flask.request.form.get("theme", configs.DEFAULT_THEME)
-            else:
-                if flask.request.form.get(cookie) is not None:
-                    value = "True"
-                else:
-                    value = "False"
+        theme = flask.request.form.get("theme", configs.DEFAULT_THEME)
 
-            response.set_cookie(cookie, value, expires=datetime.now() + timedelta(days=365))
+        if theme not in configs.THEMES:
+            theme = configs.DEFAULT_THEME
+
+        response.set_cookie("theme", theme, expires=datetime.now() + timedelta(days=365))
+
+        for cookie in configs.DEFAULT_COOKIES:
+            if cookie != "theme":
+                value = "True" if flask.request.form.get(cookie) else "False"
+                response.set_cookie(cookie, value, expires=datetime.now() + timedelta(days=365))
 
         return response
 
-    return flask.render_template(
-        "settings.html",
-        selected_theme=flask.request.cookies.get("theme", configs.DEFAULT_THEME),
-        **{
-            cookie: flask.request.cookies.get(cookie, configs.DEFAULT_COOKIES[cookie])
-            for cookie in configs.DEFAULT_COOKIES
-        },
-    )
+    cookies = {
+        cookie: flask.request.cookies.get(cookie, default_value)
+        for cookie, default_value in configs.DEFAULT_COOKIES.items()
+    }
+
+    return flask.render_template("settings.html", selected_theme=cookies.pop("theme"), **cookies)
 
 
 @main.app.errorhandler(404)
